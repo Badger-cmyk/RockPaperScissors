fb097d3ad8cfbbf2a27e938b8683f802e2bd44f9
Eben <ebenbadger16@gmail.com>
2024-08-27 18:22:47 +0000

    created a getComputerChoice function
    to return random choices(rock, paper or scissors) against a human player

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+//computer choice
+//the function getComputerChoice to randomly return rock, paper or scissors
+
+function getComputerChoice(){
+    let number = Math.random();
+
+    if(number <= 0.3){
+        console.log(number)
+        return "rock"
+    }
+    else if(number > 0.3 && number <= 0.6){
+        console.log(number)
+        return "paper"
+    }
+    else {
+        console.log(number)
+        return "scissors"
+    }
+}
+
+
+console.log(getComputerChoice())
