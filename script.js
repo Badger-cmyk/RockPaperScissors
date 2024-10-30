2efcbf252139749b1d574d6fe32e02ae62fa23fe
Eben <ebenbadger16@gmail.com>
2024-10-30 15:54:22 +0000

    added user interface to make players play the game with the buttons rather than in the console

--- script.js
+++ script.js
@@ -22,58 +22,103 @@ const getHumanChoice = () => {
 let humanScore = 0
 let computerScore = 0
 
-const playGame = () => {
-    const playRound = (humanChoice, computerChoice) => {  
-       let result = ''
-     
-        if(humanChoice === computerChoice){
-            result = "It's a tie!"
-        }
-        else {
-            switch(humanChoice){
-                case 'rock':
-                    result = (computerChoice === 'scissors') ? "You win! Rock beats Scissors" : "You lose! Paper beats Rock."
-                    break;
-                case 'paper':
-                    result = (computerChoice === 'rock') ? "You win! Paper beats Rock" : "You lose! Scissors beats Paper."
-                    break;
-                case 'scissors':
-                    result = (computerChoice === 'paper') ? "You win! Scissors beats Paper" : "You lose! Rock beats Scissors."
-                    break;
-                default:
-                    result = `Invalid option "${humanChoice.toUpperCase()}". Kindly try again.`
-            }
-        }
-     
-        console.log(`You: ${humanChoice}`)
-        console.log(`Computer: ${computerChoice}`)
-        console.log(result)
-     
-        if(result.includes('win')) humanScore++;
-        if(result.includes('lose')) computerScore++;
-        if(result.includes('tie')) {
-            humanScore++;
-            computerScore++;
-        } 
-     
-        console.log(`You: ${humanScore}`)
-        console.log(`Computer: ${computerScore}`)    
-    }
-    
-    playRound(getHumanChoice(),getComputerChoice())
-    playRound(getHumanChoice(),getComputerChoice())
-    playRound(getHumanChoice(),getComputerChoice())
-    playRound(getHumanChoice(),getComputerChoice())
-    playRound(getHumanChoice(),getComputerChoice())
+const choices = document.querySelector('.choices')
+const rockBtn = document.querySelector('.rock')
+const paperBtn = document.querySelector('.paper')
+const scissorsBtn = document.querySelector('.scissors')
 
-    if(humanScore > computerScore){
-        console.log(`Final Score: You: ${humanScore} | Computer: ${computerScore}`)
-        console.log('You Win The Game!')
+choices.addEventListener('click', (e) => {
+    const target = e.target.id
+    let playerSelection;
+
+    switch(target){
+        case 'rock':
+            playerSelection = 'rock'
+            break
+        case 'paper':
+            playerSelection = 'paper'
+            break
+        case 'scissors':
+            playerSelection = 'scissors'
+            break
     }
-    else{
-        console.log(`Final Score: You: ${humanScore} | Computer: ${computerScore}`)
-        console.log('You Lose The Game!')
+
+    
+    playRound(playerSelection)
+})
+
+const playRound = (playerSelection) => {
+    const computerSelection = getComputerChoice()
+
+    let result = ''
+
+    if(playerSelection === computerSelection){
+        result = "It's a tie!"
     }
+    else {
+        switch(playerSelection){
+            case 'rock':
+                result = (computerSelection === 'scissors') ? "You win! Rock crushes Scissors" : "You lose! Paper covers Rock."
+                break;
+            case 'paper':
+                result = (computerSelection === 'rock') ? "You win! Paper covers Rock" : "You lose! Scissors cuts Paper."
+                break;
+            case 'scissors':
+                result = (computerSelection === 'paper') ? "You win! Scissors cuts Paper" : "You lose! Rock crushes Scissors."
+                break;
+            }
+    }
+
+    if(result.includes('win')) humanScore++;
+    if(result.includes('lose')) computerScore++;
+    if(result.includes('tie')) {
+        humanScore++;
+        computerScore++;
+    } 
+
+    const displayScore = () => {
+        const display = document.querySelector('.score')
+        const buttons = document.querySelectorAll('button')
+
+        display.style.height = '300px'
+        display.style.backgroundColor = 'burlywood'
+        display.style.width = '300px'
+        display.style.padding = '10px'
+        display.style.marginTop = '10px'
+        display.style.borderRadius = '5px'
+              
+        display.innerText = `
+        Player: ${playerSelection}
+        Computer: ${computerSelection}
+
+        ${result}
+
+        Score
+        Player: ${humanScore}
+        Computer: ${computerScore}
+
+        `
+        if(humanScore === 5 || computerScore === 5){
+            if(humanScore === 5){
+                display.innerText += `You Win The Game.Congrats!.
+
+
+                Kindly Reload The Page To Play Again!
+                `
+            }
+            else {
+                display.innerText += `Computer Wins The Game!
+
+
+                Kindly Reload The Page To Play Again!
+                `
+            }   
+            
+            buttons.forEach((button) => button.disabled = true)   
+        }    
+    }
+    displayScore()
+
 }
 
-playGame()
+
